b0a6dd4452f8e4306b87035d01404dd2f4cdbde0
Taweshal Dev Thakur <143280866+taweshaldev99@users.noreply.github.com>
2023-11-26 09:02:14 +0545

    Add files via upload

---
+++ varying size of data types.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+  int main()
+  {
+  	 float a=39.4;
+  	 long double c=39.4;
+  	 cout<<"The size of 39.4 is "<<sizeof(39.4)<<endl;
+  	 cout<<"The size of 39.4f is "<<sizeof(39.4f)<<endl;
+  	 cout<<"The size of 39.4F is "<<sizeof(39.4F)<<endl;
+  	 cout<<"The size of 39.4L is "<<sizeof(39.4L)<<endl;
+  	 cout<<"The size of 39.4l is "<<sizeof(39.4l)<<endl;
+  	 return 0;
+  }
