b07d03ff73998b39d870d1a97246bb59f3604a65
Taweshal Dev Thakur <143280866+taweshaldev99@users.noreply.github.com>
2024-01-21 19:55:50 +0545

    Add files via upload

---
+++ copy constructor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ 
+ class Tem{
+ 	private: 
+ 	  int l;
+ 	public:
+ 	   Tem()
+ 	   {
+ 	   	l=90;
+ 	   	cout<<"The height is: "<<endl;
+ 	   	cout<<"Height "<<l<<endl;
+		}
+ };
+ int main()
+ {
+ 	Tem obj1;
+ 	return 0;
+ }
